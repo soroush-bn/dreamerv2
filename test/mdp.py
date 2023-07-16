828f9f2ebe28b204467ee84543d3b172c28b03b5
soroush-bn <soroush.baghernezhad3@gmail.com>
2023-07-16 13:04:54 +0430

    changing mdp.py for training gym atari pong

--- test/mdp.py
+++ test/mdp.py
@@ -1,26 +1,28 @@
-import wandb
 import argparse
 import os
-import torch
-import numpy as np
-import gym
-import os
 import sys
+
+import gym
+import numpy as np
+import torch
+import wandb
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from dreamerv2.utils.wrapper import GymMinAtar, OneHotAction,GymMinAtarCompact
+from dreamerv2.utils.wrapper import OneHotAction, DeepMindWrapperPong
 from dreamerv2.training.config import MinAtarConfig
 from dreamerv2.training.trainer import Trainer
 from dreamerv2.training.evaluator import Evaluator
+
 
 def main(args):
     wandb.login()
     env_name = args.env
     exp_id = args.id
     compact = True if args.compact == 1 else False
-    real_gym = True if args.gym== 1 else False
+    real_gym = True if args.gym == 1 else False
     '''make dir for saving results'''
     result_dir = os.path.join('results', '{}_{}'.format(env_name, exp_id))
-    model_dir = os.path.join(result_dir, 'models')                                                  #dir to save learnt models
+    model_dir = os.path.join(result_dir, 'models')  # dir to save learnt models
     os.makedirs(model_dir, exist_ok=True)
 
     np.random.seed(args.seed)
@@ -32,10 +34,11 @@ def main(args):
         device = torch.device('cpu')
     print('using :', device)
 
-    if compact:
-        env = OneHotAction(GymMinAtarCompact(env_name))
-    else:
-        env = OneHotAction(GymMinAtar(env_name))
+    # if compact:
+    #     env = OneHotAction(GymMinAtarCompact(env_name))
+    # else:
+    #     env = OneHotAction(GymMinAtar(env_name))
+    env = OneHotAction(DeepMindWrapperPong(gym.make("Pong-v0")))
     obs_shape = env.observation_space.shape
     action_size = env.action_space.shape[0]
     obs_dtype = bool
@@ -47,18 +50,18 @@ def main(args):
         env=env_name,
         obs_shape=obs_shape,
         action_size=action_size,
-        obs_dtype = obs_dtype,
-        action_dtype = action_dtype,
-        seq_len = seq_len,
-        batch_size = batch_size,
-        model_dir=model_dir, 
+        obs_dtype=obs_dtype,
+        action_dtype=action_dtype,
+        seq_len=seq_len,
+        batch_size=batch_size,
+        model_dir=model_dir,
     )
 
     config_dict = config.__dict__
     trainer = Trainer(config, device)
-    evaluator = Evaluator(config, device,compact,real_gym)
+    evaluator = Evaluator(config, device, compact, real_gym)
     wandb_name = 'dreamerv2-' + exp_id
-    with wandb.init(entity="soroush-baghernezhad3",project=wandb_name, config=config_dict):
+    with wandb.init(entity="soroush-baghernezhad3", project=wandb_name, config=config_dict):
         """training loop"""
         print('...training...')
         train_metrics = {}
@@ -71,16 +74,16 @@ def main(args):
         scores = []
         best_mean_score = 0
         best_save_path = os.path.join(model_dir, 'models_best.pth')
-        
-        for iter in range(1, trainer.config.train_steps):  
-            if iter%trainer.config.train_every == 0:
+
+        for iter in range(1, trainer.config.train_steps):
+            if iter % trainer.config.train_every == 0:
                 train_metrics = trainer.train_batch(train_metrics)
-            if iter%trainer.config.slow_target_update == 0:
-                trainer.update_target()                
-            if iter%trainer.config.save_every == 0:
+            if iter % trainer.config.slow_target_update == 0:
+                trainer.update_target()
+            if iter % trainer.config.save_every == 0:
                 trainer.save_model(iter)
             with torch.no_grad():
-                embed = trainer.ObsEncoder(torch.tensor(obs, dtype=torch.float32).unsqueeze(0).to(trainer.device))  
+                embed = trainer.ObsEncoder(torch.tensor(obs, dtype=torch.float32).unsqueeze(0).to(trainer.device))
                 _, posterior_rssm_state = trainer.RSSM.rssm_observe(embed, prev_action, not done, prev_rssmstate)
                 model_state = trainer.RSSM.get_model_state(posterior_rssm_state)
                 action, action_dist = trainer.ActionModel(model_state)
@@ -94,18 +97,18 @@ def main(args):
             if done:
                 trainer.buffer.add(obs, action.squeeze(0).cpu().numpy(), rew, done)
                 train_metrics['train_rewards'] = score
-                train_metrics['action_ent'] =  np.mean(episode_actor_ent)
+                train_metrics['action_ent'] = np.mean(episode_actor_ent)
                 wandb.log(train_metrics, step=iter)
                 scores.append(score)
-                if len(scores)>100:
+                if len(scores) > 100:
                     scores.pop(0)
                     current_average = np.mean(scores)
-                    if current_average>best_mean_score:
-                        best_mean_score = current_average 
+                    if current_average > best_mean_score:
+                        best_mean_score = current_average
                         print('saving best model with mean score : ', best_mean_score)
                         save_dict = trainer.get_save_dict()
                         torch.save(save_dict, best_save_path)
-                
+
                 obs, score = env.reset(), 0
                 done = False
                 prev_rssmstate = trainer.RSSM._init_rssm_state(1)
@@ -120,12 +123,12 @@ def main(args):
     '''evaluating probably best model'''
     evaluator.eval_saved_agent(env, best_save_path)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     """there are tonnes of HPs, if you want to do an ablation over any particular one, please add if here"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env",default='pong', type=str, help='mini atari env name')
-    parser.add_argument("--id", type=str, default='0', help='Experiment ID')
+    parser.add_argument("--env", default='pong', type=str, help='mini atari env name')
+    parser.add_argument("--id", type=str, default='realatari-nt', help='Experiment ID')
     parser.add_argument('--seed', type=int, default=123, help='Random seed')
     parser.add_argument('--device', default='cuda', help='CUDA or CPU')
     parser.add_argument('--batch_size', type=int, default=50, help='Batch size')
